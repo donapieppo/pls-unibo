037dda0e7a2dd5e87f6f1d8abc9ddbbebc383207
Donapieppo <donapieppo@yahoo.it>
2021-04-12 11:15:48 +0200

    f**k css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,9 +11,10 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import '../../stylesheets/application.scss';
+//import '../../stylesheets/application.scss';
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+
 
 
