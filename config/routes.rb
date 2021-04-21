50c8197c9f68dc9bfb53e3abbde50b11ce7b1a01
Pietro <donapieppo@yahoo.it>
2021-04-21 18:02:59 +0200

    contats and speakers

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
   resources :organizations
   resources :areas
+  
   resources :projects do
     resources :editions
     get  :choose_contact, on: :member
     post :add_contact, on: :member
+    delete :remove_contact, on: :member
   end
+
   resources :editions do
     resources :events
     get  :choose_contact, on: :member
     post :add_contact, on: :member
-    post :add_speaker, on: :member
+    delete :remove_contact, on: :member
   end
   resources :events do
     get  :choose_contact, on: :member
     post :add_contact, on: :member
-    post :add_speaker, on: :member
+    delete :remove_contact, on: :member
   end
 
   resources :contacts
