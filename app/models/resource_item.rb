bdf51b1e886fd7ca9cf3ed2df9d5f9f687db574a
Pietro <donapieppo@yahoo.it>
2021-04-23 11:36:28 +0200

    embed_url

--- app/models/resource_item.rb
+++ app/models/resource_item.rb
@@ -4,4 +4,13 @@ class ResourceItem < ApplicationRecord
   def to_s
     self.name
   end
+
+  def embed_url
+    if url =~ /https:\/\/www\.youtube\.com\/watch\?v=([0-9a-zA-Z]+)/
+      return "https://www.youtube.com/embed/#{$1}"
+
+    elsif url =~ /https:\/\/youtu.be\/([0-9a-zA-Z]+)/
+      return "https://www.youtube.com/embed/#{$1}"
+    end
+  end
 end
