02e46432724fb088fd452dca36ad0ddc560be932
Pietro <donapieppo@yahoo.it>
2021-05-06 09:47:47 +0200

    EditionComponent

---
+++ app/components/edition_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class EditionComponent < ViewComponent::Base
+  def initialize(edition:)
+    @edition = edition
+    @events = @edition.events.order(:start_date).all  
+  end
+end
