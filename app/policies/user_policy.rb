369a8fb24ea660c9db15c0c1bb919069b320f017
Pietro <donapieppo@yahoo.it>
2021-05-21 11:39:40 +0200

    user show

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,10 +1,18 @@
 class UserPolicy < ApplicationPolicy
+  def show?
+    @user && ((@user == @record) || @user.staff?)
+  end
+
   def edit?
     @user && @user.staff?
   end
 
   def update?
     @user && ((@user == @record) || @user.staff?)
+  end
+
+  def me?
+    @user
   end
 
   def myedit?
