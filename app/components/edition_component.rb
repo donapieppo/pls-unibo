fabe93e6fb3b461c2c0c014b846d135719404e50
Pietro <donapieppo@yahoo.it>
2021-06-15 15:36:07 +0200

    title_if_different_from

--- app/components/edition_component.rb
+++ app/components/edition_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class EditionComponent < ViewComponent::Base
-  def initialize(edition:)
+  def initialize(edition:, title_if_different_from:)
     @edition = edition
     @events = @edition.events.order(:start_date).all  
     @resources = @edition.resources
+    @title_if_different_from = title_if_different_from
   end
 end
