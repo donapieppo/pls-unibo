50dd9a13f80b92a17f5910782b7197f9d5f0a133
Pietro <donapieppo@yahoo.it>
2021-06-17 11:19:53 +0200

    short

--- app/components/edition_component.rb
+++ app/components/edition_component.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class EditionComponent < ViewComponent::Base
-  def initialize(edition:, title_if_different_from:)
+  def initialize(edition:, short: false, title_if_different_from: false)
     @edition = edition
+    @short = short
+    @title_if_different_from = title_if_different_from
+
     @events = @edition.events.order(:start_date).all  
     @resources = @edition.resources
-    @title_if_different_from = title_if_different_from
   end
 end
