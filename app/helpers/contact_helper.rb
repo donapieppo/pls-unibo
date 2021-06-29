40bdc24f9b94662edacb71d3149b985757e69905
Pietro <donapieppo@yahoo.it>
2021-06-29 12:42:46 +0200

    modal

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,7 +1,7 @@
 module ContactHelper
   def contacts_to_s(contacts)
     contacts.each do |contact|
-      concat(mail_to(contact.email, contact.name) + " <small>(".html_safe + contact.email + ")</small>".html_safe)
+      concat(mail_to(contact.email, contact.cn) + " <small>(".html_safe + contact.email + ")</small>".html_safe)
     end
   end
 end
