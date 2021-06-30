005b9a1252398712a99b8ac712099f3c614924af
Pietro <donapieppo@yahoo.it>
2021-06-30 13:52:36 +0200

    booking

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,8 +1,12 @@
 class BookingPolicy < ApplicationPolicy
   def create?
-    return false unless (@user && @record.activity.bookable_now?)
+    return false unless @record.activity.bookable_now?
 
-    ! @record.activity.any_cluster_siblings_booked?(@user)
+    if @user
+      ! @record.activity.any_cluster_siblings_booked?(@user)
+    else
+      true
+    end
   end
 
   def new_user?
