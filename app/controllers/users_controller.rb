e06ade7c618e2d88d13de9a2d3e9ec2c8e72db3a
Pietro <donapieppo@yahoo.it>
2021-07-06 13:11:32 +0200

    user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def show 
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
   def edit
     @user = User.find(params[:id])
     authorize @user
