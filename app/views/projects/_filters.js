235b99c2154299fda4aa7bcc43b44827060f1a02
Pietro <donapieppo@yahoo.it>
2021-07-19 11:38:46 +0200

    noresults

--- app/views/projects/_filters.js
+++ app/views/projects/_filters.js
@@ -5,6 +5,8 @@ const activity_type_selector = document.getElementById('activity_types');
 
 const hide_projects_not_selected = () => {
   document.querySelectorAll('.project').forEach ((p) => { p.style.display = 'block' });
+  noresults = document.getElementById('noresults');
+  noresults.style.display = 'block';
 
   [year_selector, audience_selector, area_selector, activity_type_selector].forEach((selector) => {
     var checked_ids = Array.prototype.map.call(selector.querySelectorAll('input:checked'), (x) => parseInt(x.value));
@@ -13,8 +15,6 @@ const hide_projects_not_selected = () => {
     if (checked_ids.length > 0) {
       document.querySelectorAll('.project').forEach( (p) => {
         var ids_in_project = p.getAttribute(`data-${selector.id}`);
-        // console.log(p);
-        // console.log(ids_in_project);
         // if intersection of checked_ids and ids_in_project empty => hide
         if (checked_ids.filter(x => ids_in_project.includes(x)).length === 0) {
           p.style.display = 'none';
@@ -22,6 +22,12 @@ const hide_projects_not_selected = () => {
       });
     };
   });
+  // if something visible hide noresult
+  document.querySelectorAll('.project').forEach ((p) => {
+    if (p.style.display == 'block') {
+      noresults.style.display = 'none';
+    }
+  })
 };
 
 [year_selector, audience_selector, area_selector, activity_type_selector].forEach( (selector) => {
