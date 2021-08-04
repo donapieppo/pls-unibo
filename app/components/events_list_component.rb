6650ddd44c400135f083b0ff9bcc4b0ea1d78cf4
Pietro <donapieppo@yahoo.it>
2021-08-04 16:07:31 +0200

    with_image

--- app/components/events_list_component.rb
+++ app/components/events_list_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class EventsListComponent < ViewComponent::Base
-  def initialize(events:, short: false)
+  def initialize(events:, short: false, with_image: false)
     @events = events
     @short = short
+    @with_image = with_image
   end
 end
 
