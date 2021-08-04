10c5f18e76e03ba53b261b0fb5c377dc9d1a263b
Pietro <donapieppo@yahoo.it>
2021-08-04 16:06:25 +0200

    with_image

--- app/components/edition_component.rb
+++ app/components/edition_component.rb
@@ -5,6 +5,7 @@ class EditionComponent < ViewComponent::Base
     @edition = edition
     @short = short
     @skip_title = skip_title || (title_if_different_from && @edition.name == @title_if_different_from)
+    @with_image = with_image
 
     @events = @edition.events.order(:start_date).all  
     @resources = @edition.resources
