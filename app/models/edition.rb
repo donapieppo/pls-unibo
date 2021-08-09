7647734600a705c741ec10eb8774c9f7682544b8
Pietro <donapieppo@yahoo.it>
2021-08-09 15:46:36 +0200

    sql sql sql

--- app/models/edition.rb
+++ app/models/edition.rb
@@ -22,4 +22,18 @@ class Edition < Activity
   def self.all_academic_years
     @@all_academic_years ||= Edition.select(:academic_year).order(academic_year: :desc).group(:academic_year).map(&:academic_year)
   end
+
+  def self.on_going_ids
+    ActiveRecord::Base.connection.execute("select a1.id, MIN(a2.start_date) as min , MAX(a2.start_date) as max from activities as a1 left join activities as a2 on a1.id = a2.parent_id where a1.type='Edition' and a2.type='Event' group by a2.parent_id having min < NOW() and max > NOW()").map(&:first)
+  end
+
+  def self.on_going_project_ids
+    ActiveRecord::Base.connection.execute("select a1.parent_id, MIN(a2.start_date) as min , MAX(a2.start_date) as max from activities as a1 left join activities as a2 on a1.id = a2.parent_id where a1.type='Edition' and a2.type='Event' group by a2.parent_id having min < NOW() and max > NOW()").map(&:first)
+  end
+
+  def self.bookable_project_ids
+    from_events = ActiveRecord::Base.connection.execute("select distinct(a1.parent_id) from activities as a1 left join activities as a2 on a1.id = a2.parent_id where a1.type='Edition' and a2.type='Event' and a2.booking_start < NOW() and a2.booking_end > NOW()").map(&:first)
+    from_edition = ActiveRecord::Base.connection.execute("select distinct(parent_id) from activities where type='Edition' and booking_start < NOW() and booking_end > NOW()").map(&:first)
+    (from_events + from_edition).uniq
+  end
 end
