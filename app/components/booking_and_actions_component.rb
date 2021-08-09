8996653658c71c9013e18182af5415b09bfc9835
Pietro <donapieppo@yahoo.it>
2021-08-09 13:24:30 +0200

    user

--- app/components/booking_and_actions_component.rb
+++ app/components/booking_and_actions_component.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class BookingAndActionsComponent < ViewComponent::Base
-  def initialize(what:)
+  def initialize(what:, user:)
     @what = what
+    @user = user
+    @booking = @user ? what.bookings.where(user_id: @user.id).first : nil
   end
 end
 
