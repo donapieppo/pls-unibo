40dc23a93b52054253cb1cd5be665bc485b8ea24
Pietro <donapieppo@yahoo.it>
2021-09-07 15:37:49 +0200

    short

--- app/components/booking_component.rb
+++ app/components/booking_component.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 class BookingComponent < ViewComponent::Base
-  def initialize(what:, user:)
+  def initialize(what:, user:, short: false)
     @what = what
     @user = user
+    @short = short
+    if @what.over?
+      return ""
+    end
     @user_booking = @user ? what.bookings.where(user_id: @user.id).first : nil
   end
 end
