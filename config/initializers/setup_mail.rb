02f16db6d31b970aa21840c3b45797267c80a2d5
Pietro <donapieppo@yahoo.it>
2021-09-13 09:23:55 +0200

    setup_mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -17,16 +17,7 @@ class DevelopmentMailInterceptor
   end  
 end  
 
-# Rails.application.reloader.to_prepare do
-#   ActionMailer::Base.smtp_settings = {
-#     address:        'mail.unibo.it',
-#     port:           587,
-#     user_name:      ENV['UNIBO_SMTP_USERNAME'],
-#     password:       ENV['UNIBO_SMTP_PASSWORD'],
-#     authentication: :login,
-#     enable_starttls_auto: true
-#   }
-
-#   ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
-#   Rails.application.routes.default_url_options[:host] = Rails.configuration.host
-# end
+Rails.application.reloader.to_prepare do
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
+  Rails.application.routes.default_url_options[:host] = Rails.configuration.host
+end
