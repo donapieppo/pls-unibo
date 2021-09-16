24542b7b9931a3ad539583dcb014ca45b476f437
Pietro <donapieppo@yahoo.it>
2021-09-16 10:10:01 +0200

    cache

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -1,7 +1,5 @@
 module ProjectHelper
   def project_academic_years_to_s(project)
-    # FIXME
-    project.cache_years or project.edition_years 
     if project.cache_years.size > 1
       "dall'" + academic_year(project.cache_years.first) + " all'" + academic_year(project.cache_years.last)
     elsif project.cache_years.size == 1
