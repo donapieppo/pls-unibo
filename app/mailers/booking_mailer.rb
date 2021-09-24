dd932613c8b662af1d783e3fd7c702b6d7e3d3e3
Pietro <donapieppo@yahoo.it>
2021-09-24 17:19:48 +0200

    booking

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,5 +1,7 @@
 class BookingMailer < ApplicationMailer
   def notify_registration(booking)
+    @booking = booking
+    @activity = booking.activity
     mail to: booking.email, subject: "Registrazione evento PLS"
   end
 end
