dd5e31933ed425521e6c2b29c2192270eafa4726
Pietro <donapieppo@yahoo.it>
2021-09-28 14:48:38 +0200

    app/components/free_seat_component.html.erb

--- app/components/free_seat_component.rb
+++ app/components/free_seat_component.rb
@@ -3,9 +3,7 @@
 class FreeSeatComponent < ViewComponent::Base
   def initialize(activity:)
     @activity = activity
-    if @activity.seats.to_i > 0
-      @free_seats = @activity.seats - @activity.bookings.sum(:seats)
-    end
+    @free_seats = @activity.free_seats
   end
 end
 
