3e4195cafde5188878472195f6459205d9dbf3ed
Pietro <donapieppo@yahoo.it>
2021-11-11 10:30:38 +0100

    Current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
 
+  before_action :log_current_user
   after_action :verify_authorized
 
   include Pundit
+
+  private 
+
+  def log_current_user
+    if current_user
+      logger.info "Current user: #{current_user.email}" 
+    end
+  end
 end
