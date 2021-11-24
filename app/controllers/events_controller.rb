cb7ea7b2d96efa0b885a1dc79f3f72c7064d4780
Pietro <donapieppo@yahoo.it>
2021-11-24 13:14:13 +0100

    atomic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -63,7 +63,7 @@ class EventsController < ApplicationController
   # Fixme booking_url only if external
   def event_params
     h = [:name, :description, :academic_year, :place, :google_map, :start_date, :duration, :seats, :sofia, :pcto]
-    unless @event.edition.atomic
+    unless @event && @event.edition.atomic
       h += [:bookable, :booking_url, :booking_start, :booking_end]
     end
     params[:event].permit(h)
