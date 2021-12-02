01c0b828e35ca0cb81172d0364b9336d84c3135e
Pietro <donapieppo@yahoo.it>
2021-12-02 15:56:48 +0100

    hidden in policy

--- app/policies/edition_policy.rb
+++ app/policies/edition_policy.rb
@@ -1,4 +1,8 @@
 class EditionPolicy < ActivityPolicy
+  def show?
+    @record.visible? || (@user && @user.staff?)
+  end
+
   def destroy?
     edit? && @record.events.empty?
   end
--- app/policies/event_policy.rb
+++ app/policies/event_policy.rb
@@ -1,4 +1,8 @@
 class EventPolicy < ActivityPolicy
+  def show?
+    @record.visible? || (@user && @user.staff?)
+  end
+
   def book?
     BookingPolicy.new(@user, @record.bookings.new).create?
   end
