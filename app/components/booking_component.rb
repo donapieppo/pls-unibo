2bd12d13b37a0a6073f6417ab9bc8cf59c3673a9
Pietro <donapieppo@yahoo.it>
2021-12-20 13:39:34 +0100

    ookable_by_user

--- app/components/booking_component.rb
+++ app/components/booking_component.rb
@@ -3,13 +3,14 @@
 class BookingComponent < ViewComponent::Base
   def initialize(booking, user, short: false)
     @booking = booking
+    @user = user
+
     @what = @booking.activity
 
     @general_bookable = @what.bookable? && @what.booking_start && @what.booking_end
-    @bookable = BookingPolicy.new(user, @booking).create?
+    @bookable_by_user = BookingPolicy.new(@user, @booking).create?
     @free_seats = @what.free_seats
 
-    @user = user
     @user_this_booking = @user ? @what.bookings.where(user_id: @user.id).first : nil
     @user_sibling_booking_activity_id = @user ? @what.cluster_siblings_booked_activity_ids(@user).first : nil
     @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
