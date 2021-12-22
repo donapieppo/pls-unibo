07cdf1b6a74f149321dbed3a076f564f690fe946
Pietro <donapieppo@yahoo.it>
2021-12-22 09:33:59 +0100

    no_area_filter , in_evidence

---
+++ app/components/projects_list_component.rb
@@ -0,0 +1,9 @@
+class ProjectsListComponent < ViewComponent::Base
+  def initialize(projects, no_filter: false, no_area_filter: false, in_evidence: false)
+    @projects = projects
+    @no_filter = no_filter
+    @no_area_filter = no_area_filter
+    @in_evidence = in_evidence
+  end
+end
+
