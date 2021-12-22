93b9409cbd5c3c5874c3e93bcca5b20c9d044d34
Pietro <donapieppo@yahoo.it>
2021-12-22 10:35:20 +0100

    media/images -> static shit

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,9 +1,11 @@
 module ImageHelper
   def area_image(area)
-    "media/images/#{area.name.downcase.gsub(' ', '')}.jpg"
+    # "media/images/#{area.name.downcase.gsub(' ', '')}.jpg"
+    "static/#{area.name.downcase.gsub(' ', '')}.jpg"
   end
 
   def area_icon(area, size: 40)
-    image_pack_tag "media/images/#{area.name.downcase.gsub(' ', '')}.png", size: size
+    # image_pack_tag "media/images/#{area.name.downcase.gsub(' ', '')}.png", size: size
+    image_pack_tag "static/#{area.name.downcase.gsub(' ', '')}.png", size: size
   end
 end
