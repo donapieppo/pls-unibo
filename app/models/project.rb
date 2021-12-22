e757a1c487627c69c3bd8e5604fbb5aabcf023d4
Pietro <donapieppo@yahoo.it>
2021-12-22 15:17:57 +0100

    optional

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < Activity
   has_and_belongs_to_many :areas,            join_table: 'activities_areas',    foreign_key: 'activity_id'
   has_and_belongs_to_many :interested_areas, join_table: 'areas_interests',     foreign_key: 'activity_id', class_name: 'Area'
   has_and_belongs_to_many :contacts,         join_table: 'activities_contacts', foreign_key: 'activity_id'
-  belongs_to :organization
+  belongs_to :organization, optional: true
 
   has_rich_text :details
 
