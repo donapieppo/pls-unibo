f3ee3a34422e7fc127239084761f476dc54dedf2
Pietro <donapieppo@yahoo.it>
2021-12-22 09:56:16 +0100

    filters

--- app/views/projects/_filters.js
+++ app/views/projects/_filters.js
@@ -3,12 +3,20 @@ const audience_selector = document.getElementById('audiences');
 const area_selector = document.getElementById('areas');
 const activity_type_selector = document.getElementById('activity_types');
 
+function clear_filters() {
+  document.querySelectorAll("#filters input[type='checkbox']").forEach((x) => x.checked = false);
+  hide_projects_not_selected();
+}
+function toggle_filters() {
+  document.getElementById("filters").classList.toggle("hidden");
+}
+
 const hide_projects_not_selected = () => {
   document.querySelectorAll('.project').forEach ((p) => { p.style.display = 'block' });
   noresults = document.getElementById('noresults');
   noresults.style.display = 'block';
 
-  [year_selector, audience_selector, area_selector, activity_type_selector].forEach((selector) => {
+  [year_selector, audience_selector, area_selector, activity_type_selector].filter(s => s !== null).forEach((selector) => {
     var checked_ids = Array.prototype.map.call(selector.querySelectorAll('input:checked'), (x) => parseInt(x.value));
     if (checked_ids.length > 0) {
       document.querySelectorAll('.project').forEach( (p) => {
@@ -28,7 +36,7 @@ const hide_projects_not_selected = () => {
   })
 };
 
-[year_selector, audience_selector, area_selector, activity_type_selector].forEach( (selector) => {
+[year_selector, audience_selector, area_selector, activity_type_selector].filter(s => s !== null).forEach( (selector) => {
   selector.addEventListener('change', () => {
     console.log(selector.id + " changed");
     hide_projects_not_selected();
