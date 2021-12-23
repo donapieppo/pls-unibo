c2c46e4c9d7c233a9d9341bae6b4f6571968f38c
Pietro <donapieppo@yahoo.it>
2021-12-23 11:31:07 +0100

    order

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss'),
-    require('tailwindcss/nesting'),
     require('postcss-nesting'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
     require('autoprefixer'),
   ],
 }
