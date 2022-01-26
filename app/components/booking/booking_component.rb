f402b49dc352a646a9249d46c10f3967414436bd
Pietro <donapieppo@yahoo.it>
2022-01-26 16:07:13 +0100

    current_user

--- app/components/booking/booking_component.rb
+++ app/components/booking/booking_component.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class Booking::BookingComponent < ViewComponent::Base
-  def initialize(booking, user, short: false)
+  def initialize(booking, current_user, short: false)
     @booking = booking
-    @user = user
+    @current_user = current_user
 
     @what = @booking.activity
 
     @general_bookable = @what.bookable? && @what.booking_start && @what.booking_end
-    @bookable_by_user = BookingPolicy.new(@user, @booking).create?
+    @bookable_by_user = BookingPolicy.new(@current_user, @booking).create?
     @free_seats = @what.free_seats
 
-    @user_this_booking = @user ? @what.bookings.where(user_id: @user.id).first : nil
-    @user_sibling_booking_activity_id = @user ? @what.cluster_siblings_booked_activity_ids(@user).first : nil
+    @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
+    @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
     @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
   end
 
