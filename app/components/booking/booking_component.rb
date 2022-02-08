68476f65e9f661cbe2efdbfd74ba29a699e90f61
Pietro <donapieppo@yahoo.it>
2022-02-08 13:13:56 +0100

    bug

--- app/components/booking/booking_component.rb
+++ app/components/booking/booking_component.rb
@@ -13,7 +13,7 @@ class Booking::BookingComponent < ViewComponent::Base
       @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
       @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
       @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
-    elsif @bookable_by_teacher_for_students = (@current_user.teacher? && what.bookable_by == 'teacher' && @what.bookable_for == 'students')
+    elsif @bookable_by_teacher_for_students = (@current_user && @current_user.teacher? && what.bookable_by == 'teacher' && @what.bookable_for == 'students')
       # ok
     else
       # future
