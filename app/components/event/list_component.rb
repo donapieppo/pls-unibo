004307c5c5e6f1678b7f7bbd8ee71bec1c500218
Pietro <donapieppo@yahoo.it>
2022-02-08 11:16:03 +0100

    past

--- app/components/event/list_component.rb
+++ app/components/event/list_component.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Event::ListComponent < ViewComponent::Base
-  def initialize(events:, short: false, with_image: false)
+  def initialize(events:, short: false, with_image: false, past: false)
     @events = events
     @short = short
     @with_image = with_image
+    @past = past
+  end
+
+  def render?
+    @events.any?
   end
 end
 
