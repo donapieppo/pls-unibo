ac932ab7270fb6f640278c7c4dfe8cdd0dc668f1
Pietro <donapieppo@yahoo.it>
2022-02-10 11:57:23 +0100

    booking list

--- app/components/booking/list_component.rb
+++ app/components/booking/list_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Booking::ListComponent < ViewComponent::Base
-  def initialize(bookings, current_user)
-    @bookings = bookings
+  def initialize(current_user, teacher: false)
     @current_user = current_user
+    @bookings = teacher ? Booking.where(teacher_id: @current_user.id) : Bookings.where(user_id: @current_user.id) 
+    @bookings = @bookings.includes(:activity, :user).order(:activity_id)
   end
 end
 
