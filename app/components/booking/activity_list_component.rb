c5f20514f0557e2ac3fa2a76f83f4d813770896f
Pietro <donapieppo@yahoo.it>
2022-02-15 09:32:32 +0100

    bookable_undone scope

--- app/components/booking/activity_list_component.rb
+++ app/components/booking/activity_list_component.rb
@@ -2,7 +2,7 @@
 
 class Booking::ActivityListComponent < ViewComponent::Base
   def initialize(current_user)
-    @activities = Activity.order('start_date desc').with_bookings
+    @activities = Activity.order('start_date desc').with_bookings.bookable_undone
     @current_user = current_user
   end
 end
