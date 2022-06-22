c3a83faafef58368ace21b9d75913aeadb7aec4a
Pietro <donapieppo@yahoo.it>
2022-06-22 12:02:58 +0200

    rails 7 link_to_delete

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -13,18 +13,19 @@ module LinkHelper
     end
   end
 
-  def link_to_delete(url, button: false)
-    link_to url, method: :delete, title: 'elimina', data: { confirm: 'Siete sicuri di voler cancellare?' } do 
+  def link_to_delete(name = "", url, button: false, _class: '')
+    button_to url, method: :delete, title: 'elimina', form: { data: { 'turbo-confirm': 'Siete sicuri di voler cancellare?'}, 
+                                                              class: "inline-block px-0 mx-0 #{_class}" } do
       '<i class="far fa-trash-alt"></i>'.html_safe
     end
   end
 
-  def link_to_remove(item, parent)
-    # remove_contact_project_contact_path
-    url = [:remove_contact, parent, contact_id: item.id]
-    link_to url, method: :delete, title: 'elimina', data: { confirm: 'Siete sicuri di voler cancellare?' } do 
-      '<i class="far fa-trash-alt"></i>'.html_safe
-    end
-  end
+  # def link_to_remove(item, parent)
+  #   # remove_contact_project_contact_path
+  #   url = [:remove_contact, parent, contact_id: item.id]
+  #   link_to url, method: :delete, title: 'elimina', data: { confirm: 'Siete sicuri di voler cancellare?' } do 
+  #     '<i class="far fa-trash-alt"></i>'.html_safe
+  #   end
+  # end
 end
 
