70a4610b31f68c84c6fa2e587f6b997cc92da819
Pietro <donapieppo@yahoo.it>
2022-06-27 13:02:45 +0200

    small

--- app/components/modal_component.rb
+++ app/components/modal_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class ModalComponent < ViewComponent::Base
-  def initialize(title: "", hidden: false, noactions: false)
+  def initialize(title: "", hidden: false, noactions: false, small: false)
     @title = title
     @hidden = hidden
+    @small = small
     @actions = noactions ? "" : "turbo:click->turbo-modal#followLink 
                                  keyup@window->turbo-modal#closeWithKeyboard
                                  click@window->turbo-modal#closeBackground
