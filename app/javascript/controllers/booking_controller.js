459743816ec739c67fda615839f39447573948ce
Pietro <donapieppo@yahoo.it>
2022-06-30 14:39:45 +0200

    stimulus

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -1,15 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ['by', 'for', 'dates', 'url', 'inpresence', 'online']
+
   connect() {
     console.log("booking controller");
 
-    display_unless(['no', 'external'], document.getElementById('bookable_by_div'), document.getElementById('edition_bookable'));
-    display_unless(['no', 'external'], document.getElementById('bookable_for_div'), document.getElementById('edition_bookable'));
+    display_unless(['no', 'external'], this.byTarget, document.getElementById('edition_bookable'));
+    display_unless(['no', 'external'], this.forTarget, document.getElementById('edition_bookable'));
 
-    display_unless('no', document.getElementById('booking_dates'), document.getElementById('edition_bookable'));
-    display_if('external', document.getElementById('booking_url'), document.getElementById('edition_bookable'));
-    display_if_checked(document.getElementById('if_in_presence'), document.getElementById('edition_in_presence'));
-    display_if_checked(document.getElementById('if_online'), document.getElementById('edition_online'));
+    display_unless('no', this.datesTarget, document.getElementById('edition_bookable'));
+    display_if('external', this.urlTarget, document.getElementById('edition_bookable'));
+    //display_if_checked(document.getElementById('if_in_presence'), document.getElementById('edition_in_presence'));
+    //display_if_checked(document.getElementById('if_online'), document.getElementById('edition_online'));
   }
 }
