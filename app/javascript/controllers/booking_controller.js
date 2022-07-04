cb57ddba3c89de8cf4094fb56d5c54466eae73a4
Pietro <donapieppo@yahoo.it>
2022-07-04 11:45:08 +0200

    bookableType

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -1,16 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['bookableboolean', 'by', 'for', 'dates', 'url', 'inpresence', 'online']
+  static targets = ['bookableType', 'by', 'for', 'dates', 'url', 'inpresence', 'online']
 
   connect() {
     console.log("booking controller");
 
-    display_unless(['no', 'external'], this.byTarget, this.bookablebooleanTarget;
-    display_unless(['no', 'external'], this.forTarget, this.bookablebooleanTarget;
+    display_unless(['no', 'external'], this.byTarget, this.bookableTypeTarget);
+    display_unless(['no', 'external'], this.forTarget, this.bookableTypeTarget);
 
-    display_unless('no', this.datesTarget, document.getElementById('edition_bookable'));
-    display_if('external', this.urlTarget, document.getElementById('edition_bookable'));
+    display_unless('no', this.datesTarget, this.bookableTypeTarget);
+    display_if('external', this.urlTarget, this.bookableTypeTarget);
     //display_if_checked(document.getElementById('if_in_presence'), document.getElementById('edition_in_presence'));
     //display_if_checked(document.getElementById('if_online'), document.getElementById('edition_online'));
   }
