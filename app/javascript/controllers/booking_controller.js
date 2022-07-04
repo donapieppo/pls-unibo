2c8434377a128c90b81e66bc189873795451ba36
Pietro <donapieppo@yahoo.it>
2022-07-04 11:38:49 +0200

    bookableboolean

--- app/javascript/controllers/booking_controller.js
+++ app/javascript/controllers/booking_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['by', 'for', 'dates', 'url', 'inpresence', 'online']
+  static targets = ['bookableboolean', 'by', 'for', 'dates', 'url', 'inpresence', 'online']
 
   connect() {
     console.log("booking controller");
 
-    display_unless(['no', 'external'], this.byTarget, document.getElementById('edition_bookable'));
-    display_unless(['no', 'external'], this.forTarget, document.getElementById('edition_bookable'));
+    display_unless(['no', 'external'], this.byTarget, this.bookablebooleanTarget;
+    display_unless(['no', 'external'], this.forTarget, this.bookablebooleanTarget;
 
     display_unless('no', this.datesTarget, document.getElementById('edition_bookable'));
     display_if('external', this.urlTarget, document.getElementById('edition_bookable'));
