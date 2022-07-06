5c8102836b843424ac4dedc326e3b9df2e5a595f
Pietro <donapieppo@yahoo.it>
2022-07-06 11:16:58 +0200

    simplify conf

--- config/application.rb
+++ config/application.rb
@@ -8,21 +8,11 @@ Bundler.require(*Rails.groups)
 
 module PlsUnibo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
     config.time_zone = "Rome"
     config.i18n.default_locale = :it
 
-    config.hosts << "tester.dm.unibo.it"
-    config.relative_url_root = '/pls'
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "www.dm.unibo.it"
   end
 end
