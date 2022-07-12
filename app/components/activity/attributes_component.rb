0480c9a997c65ca65a9e6895a5fa83e1e83fe0e5
Pietro <donapieppo@yahoo.it>
2022-07-12 11:36:34 +0200

    moved

---
+++ app/components/activity/attributes_component.rb
@@ -0,0 +1,6 @@
+class Activity::AttributesComponent < ViewComponent::Base
+  def initialize(activity)
+    @activity = activity
+  end
+end
+
--- app/components/project/attributes_component.rb
+++
@@ -1,6 +0,0 @@
-class Project::AttributesComponent < ViewComponent::Base
-  def initialize(project)
-    @project = project
-  end
-end
-
