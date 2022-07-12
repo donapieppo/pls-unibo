30533f8922fc76aaffabc1701bd940f6998ee1e9
Pietro <donapieppo@yahoo.it>
2022-07-12 11:35:53 +0200

    moved

--- app/components/edition/card_component.rb
+++
@@ -1,8 +0,0 @@
-class Edition::CardComponent < ViewComponent::Base
-  def initialize(edition, in_evidence: false)
-    @edition = edition
-    @project = @edition.project  
-    @in_evidence = in_evidence
-  end
-end
-
