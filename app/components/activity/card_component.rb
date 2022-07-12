d86731cde663c36da28cdeaec40d9f69a51bac29
Pietro <donapieppo@yahoo.it>
2022-07-12 09:30:23 +0200

    components refactor: Edition::Card + Project::Card -> Activity::CardComponent

---
+++ app/components/activity/card_component.rb
@@ -0,0 +1,29 @@
+class Activity::CardComponent < ViewComponent::Base
+  def initialize(activity, no_filter: false, in_evidence: false)
+    @activity = activity
+    current_year = @activity.current_year? 
+    @border_color = current_year ? 'border-pls-light' : 'border-gray-200'
+    @opacity = current_year ? '' : 'opacity-60'
+    @hidden = @activity.hidden? 
+    @in_evidence = in_evidence && (! @hidden)
+
+    @project = @activity.is_a?(Project) ? @activity : @activity.project
+
+    @activity_type = @project.activity_type
+    @organization = @project.organization 
+    @campus = @project.campus 
+    @other_areas = @project.interested_areas - @project.areas
+
+    # @other_areas = @project.interested_areas - @project.areas
+
+    if @activity.is_a?(Project)
+      @filter_data = no_filter ? {} : { 
+        years: @activity.cache_years,
+        audiences: @activity.editions_audience_ids.uniq,
+        areas: (@activity.area_ids + @activity.interested_area_ids).uniq, 
+        activityTypes: (@activity.parent_id ? [@activity.parent_id] : []) 
+      } 
+    end
+  end
+end
+
