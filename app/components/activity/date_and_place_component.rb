8e35d234179c261b344d0783a74ab4df84862ae9
Pietro <donapieppo@yahoo.it>
2022-07-18 11:39:40 +0200

    no_link

--- app/components/activity/date_and_place_component.rb
+++ app/components/activity/date_and_place_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Activity::DateAndPlaceComponent < ViewComponent::Base
-  def initialize(activity)
+  def initialize(activity, no_link: false)
     @activity = activity
+    @no_link = no_link
   end
 end
