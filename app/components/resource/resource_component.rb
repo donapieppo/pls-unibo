6e1a37b33a4ce84e093979dfce3b7bcd83e08882
Pietro <donapieppo@yahoo.it>
2022-07-18 11:48:11 +0200

    size limit

--- app/components/resource/resource_component.rb
+++ app/components/resource/resource_component.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Resource::ResourceComponent < ViewComponent::Base
-  def initialize(resource)
+  def initialize(resource, small: false)
     @resource = resource
+    @small = small
+    if @small
+      @limit = 250
+    else
+      @limit = 400
+    end
   end
 end
 
