67ed423164ff6455331961b7ad2bbeda8a3da5be
Pietro <donapieppo@yahoo.it>
2022-07-18 16:34:47 +0200

    size

--- app/components/resource/resource_component.rb
+++ app/components/resource/resource_component.rb
@@ -4,11 +4,7 @@ class Resource::ResourceComponent < ViewComponent::Base
   def initialize(resource, small: false)
     @resource = resource
     @small = small
-    if @small
-      @limit = 250
-    else
-      @limit = 400
-    end
+    @limit = @small ? 250 : 400
   end
 end
 
