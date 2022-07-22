8f77727d24e576d8bfdb2a2023212c7b701b50e1
Pietro <donapieppo@yahoo.it>
2022-07-22 15:22:57 +0200

    ContactComponent

--- app/components/contact/contact_component.rb
+++ app/components/contact/contact_component.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Contact::ContactComponent < ViewComponent::Base
-  def initialize(contact)
+  def initialize(contact, current_user)
     @contact = contact
+    @current_user = current_user
+    @contactPolicy = ContactPolicy.new(@current_user, @contact)
   end
 end
 
