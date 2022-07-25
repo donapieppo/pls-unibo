782f553b80679ca1bd6d381e70acace5b9098d8d
Pietro <donapieppo@yahoo.it>
2022-07-25 13:12:17 +0200

    refactor booking component

--- app/components/booking/booking_info_component.rb
+++ app/components/booking/booking_info_component.rb
@@ -4,10 +4,11 @@ class Booking::BookingInfoComponent < ViewComponent::Base
   def initialize(what, current_user)
     @what = what
     @current_user = current_user
-    @booking = what.bookings.new
-
-    @general_bookable = @what.bookable? && @what.booking_start && @what.booking_end
     @free_seats = @what.free_seats
+  end
+
+  def render?
+    @what.bookable? && @what.booking_start && @what.booking_end
   end
 end
 
