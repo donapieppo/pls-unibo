15e739d1a30f01540e1629e26d3196dbfc603c4e
Pietro <donapieppo@yahoo.it>
2022-07-26 15:25:42 +0200

    no_filter

--- app/components/project/list_component.rb
+++ app/components/project/list_component.rb
@@ -2,7 +2,7 @@ class Project::ListComponent < ViewComponent::Base
   def initialize(projects, area_id: nil, no_filter: false, no_year_filter: false, no_area_filter: false, in_evidence: false)
     @projects = projects
     @area_id = area_id.to_i
-    @no_filter = no_filter
+    @no_filter = no_filter || @projects.empty?
     @no_year_filter = no_year_filter
     @no_area_filter = no_area_filter
     @in_evidence = in_evidence
