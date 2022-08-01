610c2b2e58c145a6dca375749ec6e62becf5b4f2
Pietro <donapieppo@yahoo.it>
2022-08-01 13:12:22 +0200

    @what.unbookable?

--- app/components/booking/booking_component.rb
+++ app/components/booking/booking_component.rb
@@ -1,26 +1,29 @@
 # frozen_string_literal: true
 
 class Booking::BookingComponent < ViewComponent::Base
-  def initialize(what, current_user, short: false)
+  def initialize(what, current_user)
     @what = what
     @current_user = current_user
-    @booking = what.bookings.new
 
-    @bookable_by_user = BookingPolicy.new(@current_user, @booking).create?
-    @free_seats = @what.free_seats
+    unless @what.unbookable?
+      @booking = what.bookings.new
 
-    if @bookable_for_itsself = (@what.bookable_for == 'itsself')
-      @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
-      @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
-      @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
-    elsif @bookable_by_teacher_for_students = (@current_user && @current_user.teacher? && what.bookable_by == 'teacher' && @what.bookable_for == 'students')
-      # ok
-    else
-      # future
+      @bookable_by_user = BookingPolicy.new(@current_user, @booking).create?
+      @free_seats = @what.free_seats
+
+      if @bookable_for_itsself = (@what.bookable_for == 'itsself')
+        @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
+        @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
+        @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
+      elsif @bookable_by_teacher_for_students = (@current_user && @current_user.teacher? && what.bookable_by == 'teacher' && @what.bookable_for == 'students')
+        # ok
+      else
+        # future
+      end
     end
   end
 
   def render?
-    ! @what.over?
+    ! @what.unbookable?
   end
 end
