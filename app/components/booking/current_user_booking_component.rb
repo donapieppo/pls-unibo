31a2eae37e2a7447ff031e96cebef9163d21cfc0
Pietro <donapieppo@yahoo.it>
2022-08-01 13:13:59 +0200

    render?

--- app/components/booking/current_user_booking_component.rb
+++ app/components/booking/current_user_booking_component.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
 
 class Booking::CurrentUserBookingComponent < ViewComponent::Base
-  def initialize(what, current_user, short: false)
+  def initialize(what, current_user)
     @what = what
     @current_user = current_user
 
-    @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
+    if @current_user
+      @user_this_booking = @current_user ? @what.bookings.where(user_id: @current_user.id).first : nil
 
-    @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
-    @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
+      @user_sibling_booking_activity_id = @current_user ? @what.cluster_siblings_booked_activity_ids(@current_user).first : nil
+      @user_sibling_booking_activity = Activity.find(@user_sibling_booking_activity_id) if @user_sibling_booking_activity_id
+    end
+  end
+
+  def render?
+    @current_user 
   end
 end
 
