bfcd906f95a0daeacd618979b91e426398c06ed1
Pietro <donapieppo@yahoo.it>
2022-08-01 11:25:23 +0200

    set_events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,21 +3,33 @@ class PagesController < ApplicationController
 
   def scienza_al_cinema
     @edition = Edition.find(392)
+    set_events
     render layout: 'pages'
   end
 
   def workshop21
     @edition = Edition.find(121)
+    set_events
     render layout: 'pages'
   end
 
   def workshop22
     @edition = Edition.find(384)
+    set_events
     render layout: 'pages'
   end
 
   def metodi_matematici_animazione
     @edition = Edition.find(282)
+    set_events
     render layout: 'application'
+  end
+
+  private
+
+  def set_events
+    @events = @edition.events.future.order(start_date: :asc).all  
+    @past_events = @edition.events.past.order(start_date: :desc).all  
+    @all_events = @events + @past_events
   end
 end
