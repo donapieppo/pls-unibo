f19c623b4b270302bfb64f8a60be4c4008b6cf70
Pietro <donapieppo@yahoo.it>
2022-09-26 12:42:19 +0200

    default seats was missing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,4 @@
 class BookingsController < ApplicationController
-  #prepend_before_action :check_captcha, :check_privacy_policy, only: [:acreate] 
   before_action :set_activity, except: %i(index thankyou confirm destroy)
   before_action :set_booking_and_check_permission, only: %i(thankyou confirm, destroy)
 
@@ -154,36 +153,11 @@ class BookingsController < ApplicationController
     elsif ! (current_user.confirmed_teacher? || current_user.staff?)
       params[:booking][:seats] = 1
       params[:booking].delete(:school_class)
+    else
+      # default seats
+      params[:booking][:seats] = 1 unless params[:booking][:seats]
     end
     params[:booking].permit(:email, :name, :surname, :role, :grade, :teacher_name, :teacher_surname, :teacher_email, :school_class,   
                             :other_string, :notes, :online, :seats)
-
-    # if current user the model adds user data
-    #if current_user
-    #  { user_id: current_user.id, 
-    #    notes: params[:booking][:notes],
-    #    online: params[:booking][:online],
-    #    seats: params[:booking][:seats] }
-    #else
-    #  logger.info("No current user")
-    #  params[:booking].permit(:email, :name, :surname, :role, :school_type, :other_string, :notes, :online, :seats)
-    #end
-  end
-
-  private
-
-  def check_captcha
-    unless verify_recaptcha
-      self.resource = resource_class.new sign_up_params
-      respond_with_navigational(resource) { render :new }
-    end 
-  end
-
-  def check_privacy_policy
-    unless params[:user][:privacy_policy] == 'true'
-      self.resource = resource_class.new sign_up_params
-      self.resource.errors[:privacy_policy] << 'Per proseguire è necessario accettare le condizioni.'
-      respond_with_navigational(resource) { render :new }
-    end
   end
 end
