e0031f53c7c71327ed9cca476ec359d300e6ab7a
Pietro <donapieppo@yahoo.it>
2022-10-10 13:19:16 +0200

    test

--- test/components/bookability/actions_test.rb
+++ test/components/bookability/actions_test.rb
@@ -9,14 +9,22 @@ class Bookability::ActionComponentTest < ViewComponent::TestCase
     assert_no_text(/[a-z]+/)
   end
 
-  test "bookings=yes without dates don't have actions" do
+  test "booking = yes without dates don't have actions" do
     u = FactoryBot.create(:user, :student_secondary)
     edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, seats: 10)
     render_inline Bookability::ActionsComponent.new(edition, u)
     assert_no_text(/[a-z]+/)
   end
 
-  test "bookings = yes for student_secondary do not have 'iscriviti' link" do
+  test "booking = yes with booking_end = yesterday do not have 'iscriviti' link" do
+    u = FactoryBot.create(:user, :student_secondary)
+    edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, seats: 10, 
+                                booking_start: Date.today - 3.days, booking_end: Date.yesterday)
+    result = render_inline Bookability::ActionsComponent.new(edition, u)
+    assert_no_link 'iscriviti'
+  end
+
+  test "booking = yes for student_secondary do not have 'iscriviti' link" do
     u = FactoryBot.create(:user, :student_university)
     edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, seats: 10,  
                                           booking_start: Date.yesterday, booking_end: Date.tomorrow)
@@ -24,19 +32,19 @@ class Bookability::ActionComponentTest < ViewComponent::TestCase
     assert_no_link 'iscriviti'
   end
 
-  test "bookings = yes for student_secondary have iscriviti for student_secondary" do
+  test "booking = yes for student_secondary have iscriviti for student_secondary" do
     u = FactoryBot.create(:user, :student_secondary)
     edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, seats: 10, 
                                           booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    result = render_inline Bookability::ActionsComponent.new(edition, u)
+    render_inline Bookability::ActionsComponent.new(edition, u)
     assert_link 'iscriviti'
   end
 
-  test "bookings = yes for student_secondary have prenota for student_secondary" do
+  test "booking = to_confirm for student_secondary have prenota for student_secondary" do
     u = FactoryBot.create(:user, :student_secondary)
     edition = FactoryBot.create(:edition, bookable: 'to_confirm', bookable_by_student_secondary: true, seats: 10, 
                                           booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    result = render_inline Bookability::ActionsComponent.new(edition, u)
+    render_inline Bookability::ActionsComponent.new(edition, u)
     assert_link 'prenota'
   end
 end
