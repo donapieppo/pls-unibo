c83ae0660acb3441afe469f7e5a6792b24b1cd54
Pietro <donapieppo@yahoo.it>
2022-10-21 12:48:05 +0200

    test

--- test/components/bookability/actions_test.rb
+++ test/components/bookability/actions_test.rb
@@ -27,40 +27,4 @@ class Bookability::ActionComponentTest < ViewComponent::TestCase
     result = render_inline Bookability::ActionsComponent.new(edition, u)
     assert_no_link 'iscriviti'
   end
-
-  test "booking = yes for student_secondary do not have 'iscriviti' link for student_university" do
-    u = FactoryBot.create(:user, :student_university)
-    edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, 
-                                          seats: 10, in_presence: true,
-                                          booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    render_inline Bookability::ActionsComponent.new(edition, u)
-    assert_no_link 'iscriviti'
-  end
-
-  test "booking = yes for student_secondary have iscriviti for student_secondary" do
-    u = FactoryBot.create(:user, :student_secondary)
-    edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_student_secondary: true, 
-                                          seats: 10, in_presence: true,
-                                          booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    render_inline Bookability::ActionsComponent.new(edition, u)
-    assert_link 'iscriviti'
-  end
-
-  test "booking = to_confirm for student_secondary have prenota for student_secondary" do
-    u = FactoryBot.create(:user, :student_secondary)
-    edition = FactoryBot.create(:edition, bookable: 'to_confirm', bookable_by_student_secondary: true, 
-                                          seats: 10, in_presence: true, 
-                                          booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    render_inline Bookability::ActionsComponent.new(edition, u)
-    assert_link 'prenota'
-  end
-
-  test "booking = yes for class and remote and in presence event can not book a class if not @seats > 0" do
-    u = FactoryBot.create(:user, :teacher)
-    edition = FactoryBot.create(:edition, bookable: 'yes', bookable_by_teacher: true, bookable_by_teacher_for_classes: true, 
-                                          seats: 1, in_presence: true,
-                                          booking_start: Date.yesterday, booking_end: Date.tomorrow)
-    render_inline Bookability::ActionsComponent.new(edition, u)
-    assert_link 'iscrivi'
-  end
 end
