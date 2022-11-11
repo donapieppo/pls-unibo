fcdc73aedf35c2c825cfc090801b9827607a2625
Pietro <donapieppo@yahoo.it>
2022-11-11 10:02:15 +0100

    refactor TODO FIXME

--- app/components/booking/user_list_component.rb
+++ app/components/booking/user_list_component.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 class Booking::UserListComponent < ViewComponent::Base
-  def initialize(activity)
-    @activity = activity
-
-    @bookings = @activity.bookings.includes(:user, :school).order(:created_at)
-    @activity_online_mails = []   
-    @activity_in_presence_mails = []   
+  def initialize(bookings)
+    @bookings = bookings
+    @online_mails = []   
+    @in_presence_mails = []   
     @teacher_mails = []
-
-    @editable = false
   end
 end
