0c9e4dd336e379c5fa1cd969536d0a402160a840
Pietro <donapieppo@yahoo.it>
2023-02-22 11:11:49 +0100

    report

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class HomeController < ApplicationController
   before_action :skip_authorization
 
@@ -30,5 +32,19 @@ class HomeController < ApplicationController
     @archive_page = "#{config.relative_url_root}/archivio/it/#{@slug}.html"
 
     render layout: 'archive'
+  end
+
+  def report
+    @editions = Edition.includes(:audience, project: [:activity_type, :areas]).order(:academic_year, :name)
+    result = CSV.generate(col_sep: "\t", quote_char: '"') do |csv|
+      @editions.each do |edition|
+        # matematica :-)
+        if edition.project.area_ids.include?(6)
+          csv << [edition.academic_year, edition.project.areas.map(&:name).join(', '), edition.project.activity_type, edition.project.name, edition.name, edition.audience]
+        end
+      end
+    end
+    #raise result.inspect
+    send_data result, filename: "report.csv", type: :csv
   end
 end
