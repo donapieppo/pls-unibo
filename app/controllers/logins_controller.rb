df1a7a931e87c1bf5f0ccbc0b0f2c6e8cc35bff5
Pietro <donapieppo@yahoo.it>
2023-09-22 09:41:38 +0200

    rubocop

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,5 +1,5 @@
 class LoginsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :developer  
+  skip_before_action :verify_authenticity_token, only: :developer
   before_action :skip_authorization
   skip_before_action :force_sso_user, raise: false
 
@@ -30,7 +30,7 @@ class LoginsController < ApplicationController
     log_unibo_omniauth
     parse_unibo_omniauth
 
-    if @email =~ /@(studio\.)?unibo.it\z/
+    if @email.match?(/@(studio\.)?unibo.it\z/)
       allow_and_create
     else
       logger.info "#{@email} user not allowed."
@@ -42,7 +42,7 @@ class LoginsController < ApplicationController
     parse_developer_omniauth
     check_developer!
     user = User.where(email: @email).first
-    if ! user
+    if !user
       logger.info "Developer Authentication: User #{@email} to be CREATED"
       user = create_logged_user
       sign_in_and_redirect user, myedit_users_path
@@ -54,9 +54,9 @@ class LoginsController < ApplicationController
 
   def check_developer!
     (Rails.env.development? || Rails.env.test?) or raise "NOT IN DEVELOPMENT"
-    request.remote_ip == '127.0.0.1' or raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
-    # request.remote_ip =~ /^192\.\d+\.\d+\.\d+/ or 
-    # request.remote_ip == '::1' or 
+    request.remote_ip == "127.0.0.1" or raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
+    # request.remote_ip =~ /^192\.\d+\.\d+\.\d+/ or
+    # request.remote_ip == '::1' or
     # request.remote_ip =~ /^172\.\d+\.\d+\.\d+/ or raise "ONLY LOCAL OF DOCKER. YOU ARE #{request.remote_ip}"
   end
 
@@ -80,39 +80,39 @@ class LoginsController < ApplicationController
     session[:user_id] = nil
     reset_session
     cookies.clear
-    redirect_to root_path, notice: "Uscito correttamente."  
+    redirect_to root_path, notice: "Uscito correttamente."
   end
 
-  private 
+  private
 
   # omniauth.auth: #<OmniAuth::AuthHash credentials=#<OmniAuth::AuthHash expires=true expires_at=... token="..."> extra=#<OmniAuth::AuthHash id_info=#<OmniAuth::AuthHash at_hash="..." aud="..." azp="..." email="donapieppo@gmail.com" email_verified=true exp=1472639186 iat=1472635586 iss="accounts.google.com" sub="..."> id_token="..." raw_info=#<OmniAuth::AuthHash email="donapieppo@gmail.com" email_verified="true" family_name="Dona" gender="male" given_name="Pieppo" kind="plus#personOpenIdConnect" locale="it" name="Pieppo Dona" picture="..." profile="..." sub="...">> info=#<OmniAuth::AuthHash::InfoHash email="donapieppo@gmail.com" first_name="Pieppo" image="https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg" last_name="Dona" name="Pieppo Dona" urls=#<OmniAuth::AuthHash Google="https://plus.google.com/104065190780467868357">> provider="google_oauth2" uid="104065190780467868357">
   def parse_google_omniauth
     # logger.info("omniauth.auth: #{request.env['omniauth.auth'].inspect}")
-    oinfo  = request.env['omniauth.auth'].info
+    oinfo = request.env["omniauth.auth"].info
     @email = oinfo.email
-    @name  = oinfo.firstname
+    @name = oinfo.firstname
     @surname = oinfo.last_name
   end
 
   def parse_developer_omniauth
-    oinfo  = request.env['omniauth.auth'].info
+    oinfo = request.env["omniauth.auth"].info
     @email = oinfo.name
-    @name  = "Pippo"
+    @name = "Pippo"
     @surname = "Pluto"
   end
 
   def parse_unibo_omniauth
-    @upn  = request.env['omniauth.auth'].uid
-    oinfo = request.env['omniauth.auth'].info
-    extra = request.env['omniauth.auth'].extra.raw_info
+    @upn = request.env["omniauth.auth"].uid
+    oinfo = request.env["omniauth.auth"].info
+    extra = request.env["omniauth.auth"].extra.raw_info
 
     @idAnagraficaUnica = extra.idAnagraficaUnica.to_i
     @idAnagraficaUnica > 0 or raise "NO idAnagraficaUnica"
 
-    @email         = @upn
-    @name          = oinfo.first_name || oinfo.name
-    @surname       = oinfo.last_name
-    @nationalpin   = extra.codiceFiscale
+    @email = @upn
+    @name = oinfo.first_name || oinfo.name
+    @surname = oinfo.last_name
+    @nationalpin = extra.codiceFiscale
   end
 
   def sign_in_and_redirect(user, url)
@@ -132,12 +132,14 @@ class LoginsController < ApplicationController
   def allow_and_create
     # user = @idAnagraficaUnica ? ::User.where(id: @idAnagraficaUnica).first : ::User.where(email: @email).first
     user = User.find_by_email(@email)
-    if ! user
+    if !user
       logger.info "Authentication: allow_and_create user #{@email} to be CREATED."
-      h = { # id:      @idAnagraficaUnica || 0,
-            email:   @email,
-            name:    @name, 
-            surname: @surname }
+      h = {
+        # id:      @idAnagraficaUnica || 0,
+        email: @email,
+        name: @name,
+        surname: @surname
+      }
       user = User.create!(h)
     end
     logger.info "allow_and_create: #{user.inspect}"
@@ -152,12 +154,12 @@ class LoginsController < ApplicationController
 
   def log_unibo_omniauth
     logger.info("Chiamato shibboleth")
-    logger.info("HTTP_EPPN: #{request.env['HTTP_EPPN']}")
+    logger.info("HTTP_EPPN: #{request.env["HTTP_EPPN"]}")
 
-    if request.env['omniauth.auth']
-      logger.info("Authentication: uid   = #{request.env['omniauth.auth'].uid}")
-      logger.info("Authentication: info  = #{request.env['omniauth.auth'].info}")
-      logger.info("Authentication: extra = #{request.env['omniauth.auth'].extra}")
+    if request.env["omniauth.auth"]
+      logger.info("Authentication: uid   = #{request.env["omniauth.auth"].uid}")
+      logger.info("Authentication: info  = #{request.env["omniauth.auth"].info}")
+      logger.info("Authentication: extra = #{request.env["omniauth.auth"].extra}")
     end
   end
 
