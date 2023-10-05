72af47a5d90bbf7ae9e1089860ecaf8ced6a6e66
Pietro <donapieppo@yahoo.it>
2023-10-05 11:47:44 +0200

    atomic and rubocop

--- app/components/event/list_component.rb
+++ app/components/event/list_component.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class Event::ListComponent < ViewComponent::Base
-  def initialize(events, current_user, short: false, with_image: true, title: nil)
+  def initialize(events, current_user, short: false, with_image: true, title: nil, atomic: false)
     @events = events
     @current_user = current_user
     @short = short
     @with_image = with_image
     @title ||= "Prossimi eventi"
+    @atomic = atomic
   end
 
   def render?
