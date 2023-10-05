f91d4f0cf5dc0f9e315e06f31160bb5e4a4a274d
Pietro <donapieppo@yahoo.it>
2023-10-05 11:49:11 +0200

    rubocop

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,10 @@
 class Event < Activity
-  has_and_belongs_to_many :contacts, through: 'activities_contacts', foreign_key: 'activity_id'
-  has_and_belongs_to_many :speakers, join_table: 'activities_speakers', foreign_key: 'activity_id', class_name: 'Contact'
+  has_and_belongs_to_many :contacts, through: "activities_contacts", foreign_key: "activity_id"
+  has_and_belongs_to_many :speakers, join_table: "activities_speakers", foreign_key: "activity_id", class_name: "Contact"
 
   has_rich_text :details
 
-  belongs_to :edition, foreign_key: 'parent_id'
+  belongs_to :edition, foreign_key: "parent_id"
   scope :bookable, -> { where("bookable != 'no' and booking_start < NOW() and booking_end > NOW()") }
 
   def to_s
