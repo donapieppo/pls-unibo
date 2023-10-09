4c42cd3eada50c8df020f740f11d6cbe65d67bca
Pietro <donapieppo@yahoo.it>
2023-10-09 15:28:05 +0200

    current_user

--- app/components/booking/user_list_component.rb
+++ app/components/booking/user_list_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Booking::UserListComponent < ViewComponent::Base
-  def initialize(bookings)
+  def initialize(bookings, current_user)
     @bookings = bookings
-    @online_mails = []   
-    @in_presence_mails = []   
+    @current_user = current_user
+    @online_mails = []
+    @in_presence_mails = []
     @teacher_mails = []
   end
 end
