c05e450961e7a008ef92dde62e01ed81798234a8
Pietro <donapieppo@yahoo.it>
2023-10-10 09:57:25 +0200

    negate

--- app/javascript/controllers/hideshow_controller.js
+++ app/javascript/controllers/hideshow_controller.js
@@ -2,13 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = {
-    display: { type: String, default: 'block' }
+    display: { type: String, default: 'block' },
+    negate: { type: Boolean, default: false }
   }
   static targets = ['control', 'controlled']
 
   connect() {
     console.log(this.displayValue)
-    display_if_checked(this.controlledTarget, this.controlTarget, this.displayValue);
+    display_if_checked(this.controlledTarget, this.controlTarget, this.displayValue, this.negate);
   }
 }
 
