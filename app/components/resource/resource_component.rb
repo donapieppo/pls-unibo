eea6d37acd64edc46df44479ba831eedcbd0230a
Pietro <donapieppo@yahoo.it>
2023-10-18 13:05:11 +0200

    size: :base, with_download: false

--- app/components/resource/resource_component.rb
+++ app/components/resource/resource_component.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
 class Resource::ResourceComponent < ViewComponent::Base
-  def initialize(resource, small: false)
+  def initialize(resource, current_user = nil, size: :base, with_download: false)
     @resource = resource
-    @small = small
-    @limit = @small ? 250 : 350
+    @current_user = current_user
+    @with_download = with_download
+
+    if @with_download
+      @link_url = if !@resource.url.blank?
+        @resource.url
+      elsif @resource.image? || @resource.document_type == "document"
+        @resource.document
+      end
+    end
+
+    @policy = ResourcePolicy.new(@current_user, @resource)
+    @small = (size == :small)
+    @limit = case size
+    when :small
+      250
+    when :base
+      350
+    when :big
+      800
+    end
   end
 end
