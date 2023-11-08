863fc222fbf289d7aca9b055bee52ca3dac55c70
Pietro <donapieppo@yahoo.it>
2023-11-08 10:19:16 +0100

    user_not_authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,23 +11,22 @@ class ApplicationController < ActionController::Base
     # session[:user_id] = User.where("name > 'm' and id>100").first.id
     if session[:user_id]
       @current_user ||= User.find(session[:user_id])
-    else
-      nil
     end
   end
 
   impersonates :user
 
-  private 
+  private
 
   def log_current_user
     if current_user
-      logger.info "Current user: #{current_user.email}" 
+      logger.info "Current user: #{current_user.email}"
     end
   end
 
-   def user_not_authorized
-     flash[:alert] = "Operazione non permessa con questo utente."
-     redirect_back(fallback_location: root_path)
+  def user_not_authorized
+    flash[:alert] = "Operazione non permessa."
+    # redirect_back(fallback_location: root_path)
+    redirect_to root_path
   end
 end
