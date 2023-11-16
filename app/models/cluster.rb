5d37158631474e791dbaaf99bcb08957253ce884
Pietro <donapieppo@yahoo.it>
2023-11-16 15:43:33 +0100

    max_bookable_activities_to_s

--- app/models/cluster.rb
+++ app/models/cluster.rb
@@ -9,4 +9,12 @@ class Cluster < ApplicationRecord
   def to_s
     self.name
   end
+
+  def max_bookable_activities_to_s
+    if max_bookable_activities.to_i == 1
+      "È possibile iscriversi a una sola delle attività."
+    elsif max_bookable_activities.to_i > 1
+      "È possibile iscriversi a non più di #{max_bookable_activities.to_i} attività."
+    end
+  end
 end
