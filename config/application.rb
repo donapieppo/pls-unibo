a2867962975d6d443e9e35db3f364466c4392f0d
Pietro <donapieppo@yahoo.it>
2024-02-16 13:31:30 +0100

    time_zone and locale

--- config/application.rb
+++ config/application.rb
@@ -14,17 +14,12 @@ module PlsUnibo
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
-    config.hosts << "www.dm.unibo.it"
-    config.action_mailer.default_url_options = { protocol: 'https' }
+    config.time_zone = "Rome"
+    config.i18n.default_locale = :it
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "www.pls.unibo.it"
+    config.action_mailer.default_url_options = {protocol: "https"}
   end
 end
