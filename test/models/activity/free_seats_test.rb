f6a06a64b43e616db97b495368b8d65a80e6661e
Pietro Donatini <pietro.donatini@unibo.it>
2025-02-13 11:08:54 +0100

    test

--- test/models/activity/free_seats_test.rb
+++ test/models/activity/free_seats_test.rb
@@ -1,11 +1,13 @@
 require "test_helper"
 
 class Activity::FreeSeatsTest < ActiveSupport::TestCase
-  test 'external bookings do not have dates' do
-    edition = FactoryBot.create(:edition, bookable: 'yes', in_presence: true, bookable_by_all: true, seats: 10,
-                                          booking_start: Date.yesterday, booking_end: Date.tomorrow)
+  test "external bookings do not have dates" do
+    edition = FactoryBot.create(:edition,
+      bookable: "yes", in_presence: true, bookable_by_all: true, seats: 10,
+      booking_start: Date.yesterday, booking_end: Date.tomorrow)
 
-    booking = FactoryBot.create(:booking, :student_secondary_booking, activity: edition)
+    _booking = FactoryBot.create(:booking, :student_secondary_booking, activity: edition)
+
     # IS CACHE, neads reload
     edition = Edition.find(edition.id)
     assert_equal 9, edition.free_seats
