19be02d82f2fd94d227f58a8172ddcb58a8f00e6
Pietro Donatini <pietro.donatini@unibo.it>
2025-02-21 13:13:28 +0100

    rubocop

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,11 +1,11 @@
 module ImageHelper
   def area_image(area)
     # "media/images/#{area.name.downcase.gsub(' ', '')}.jpg"
-    "#{area.name.downcase.gsub(' ', '')}.jpg"
+    "#{area.name.downcase.delete(" ")}.jpg"
   end
 
   def area_icon(area, size: 40)
     # image_pack_tag "media/images/#{area.name.downcase.gsub(' ', '')}.png", size: size
-    image_tag "areas/#{area.name.downcase.gsub(' ', '')}.png", size: size
+    image_tag "areas/#{area.name.downcase.delete(" ")}.png", size: size, alt: "Area icon"
   end
 end
