cd87264919a325fc938e1d3dc82967e65f27987f
Pietro Donatini <pietro.donatini@unibo.it>
2025-08-05 16:20:32 +0200

    style

--- app/components/resource_container/card_component.rb
+++ app/components/resource_container/card_component.rb
@@ -1,9 +1,8 @@
 class ResourceContainer::CardComponent < ViewComponent::Base
-  def initialize(resource_container, current_user)
+  def initialize(resource_container, current_user, compressed: false)
     @resource_container = resource_container
     @current_user = current_user
     @resource_container_policy = ResourceContainerPolicy.new(@current_user, @resource_container)
+    @compressed = compressed
   end
 end
-
-
