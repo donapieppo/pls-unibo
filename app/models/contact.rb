f53955f95571946622de0dc46cccfc67b1d66b29
Pietro Donatini <pietro.donatini@unibo.it>
2025-08-21 14:25:04 +0200

    rubocop

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,33 +1,33 @@
 class Contact < ApplicationRecord
   has_many :contact_records
-  has_many :organized_areas, foreign_key: "head_id", class_name: 'Area'
+  has_many :organized_areas, foreign_key: "head_id", class_name: "Area"
   has_and_belongs_to_many :areas
-  
+
   has_one_attached :avatar
   has_and_belongs_to_many :activities
-  has_and_belongs_to_many :activities_as_speaker, join_table: 'activities_speakers', class_name: 'Activity'
+  has_and_belongs_to_many :activities_as_speaker, join_table: "activities_speakers", class_name: "Activity"
 
   has_and_belongs_to_many :projects
   has_and_belongs_to_many :editions
   has_and_belongs_to_many :events
 
-  validates :name, :email, uniqueness: { scope: :surname, allow_blank: true }
-  validates :web_page, format: { with: URI.regexp(['http', 'https']), allow_blank: true }
+  validates :name, :email, uniqueness: {scope: :surname, allow_blank: true}
+  validates :web_page, format: {with: URI::DEFAULT_PARSER.make_regexp(["http", "https"]), allow_blank: true}
 
-  def records 
-    self.contacts_records.map(&:record)
+  def records
+    contacts_records.map(&:record)
   end
 
   def cn
-    "%s %s" % [self.name == '-' ? '' : self.name, self.surname]
+    "%s %s" % [(name == "-") ? "" : name, surname]
   end
 
   def cn_militar
-    "%s, %s" % [self.surname, self.name]
+    "%s, %s" % [surname, name]
   end
 
   def abbr
-    "%s. %s" % [self.name[0], self.surname]
+    "%s. %s" % [name[0], surname]
   end
 
   def to_s
@@ -35,6 +35,6 @@ class Contact < ApplicationRecord
   end
 
   def deletable?
-    self.activities.empty? && self.organized_areas.empty? && self.activities_as_speaker.empty?
+    activities.empty? && organized_areas.empty? && activities_as_speaker.empty?
   end
 end
