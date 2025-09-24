6ab1dde3b29c4eae3893756ec968f4652649df1d
Pietro Donatini <pietro.donatini@unibo.it>
2025-09-24 15:53:27 +0200

    factory

--- test/factories/booking.rb
+++ test/factories/booking.rb
@@ -1,19 +1,4 @@
 FactoryBot.define do
-  factory :booking do 
-    user
-    activity factory: :edition 
-  end
-
-  trait :student_secondary_booking do
-    user { FactoryBot.create(:user, :student_secondary) }
-    teacher_name { 'Teacher name in booking' }
-    teacher_surname { 'Teacher surname in booking' }
-    teacher_email { 'teacher@example.com' }
-  end
-
-  trait :data_complete do
-    teacher_name { 'Teacher name in booking' }
-    teacher_surname { 'Teacher surname in booking' }
-    teacher_email { 'teacher@example.com' }
+  factory :booking do
   end
 end
