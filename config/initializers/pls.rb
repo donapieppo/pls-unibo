bb6f98589eb1583dcf31e2ed368280b960055443
Pietro Donatini <pietro.donatini@unibo.it>
2025-10-08 09:33:49 +0200

    tolto ACCROCCHIO_VECCHIO_SITO

--- config/initializers/pls.rb
+++ config/initializers/pls.rb
@@ -2,18 +2,6 @@ CURRENT_ACADEMIC_YEAR = 2025
 CESIA_UPN = ["pietro.donatini@unibo.it", "valeria.montesi3@unibo.it"]
 BOOKING_WATCHERS = ["mirella.manaresi@unibo.it", "alessia.cattabriga@unibo.it"]
 
-ACCROCCHIO_VECCHIO_SITO = [
-  "",
-  "http://www.pls.unibo.it/it/biologia-e-biotecnologie",
-  "http://www.pls.unibo.it/it/chimica",
-  "http://www.pls.unibo.it/it/fisica",
-  "http://www.pls.unibo.it/it/geologia",
-  "http://www.pls.unibo.it/it/informatica",
-  "http://www.pls.unibo.it/it/matematica",
-  "http://www.pls.unibo.it/it/scienze-naturali-ambientali",
-  "http://www.pls.unibo.it/it/statistica"
-]
-
 SOFIA_LINK = "https://sofia.istruzione.it/"
 
 module Pls
