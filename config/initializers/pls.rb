3ad4fd64b67b6c4b291db64857fd2e2d02058a8e
Pietro Donatini <pietro.donatini@unibo.it>
2025-10-15 15:01:23 +0200

    impersonation

--- config/initializers/pls.rb
+++ config/initializers/pls.rb
@@ -2,23 +2,14 @@ CURRENT_ACADEMIC_YEAR = 2025
 CESIA_UPN = ["pietro.donatini@unibo.it", "valeria.montesi3@unibo.it"]
 BOOKING_WATCHERS = ["mirella.manaresi@unibo.it", "alessia.cattabriga@unibo.it"]
 
-ACCROCCHIO_VECCHIO_SITO = [
-  "",
-  "http://www.pls.unibo.it/it/biologia-e-biotecnologie",
-  "http://www.pls.unibo.it/it/chimica",
-  "http://www.pls.unibo.it/it/fisica",
-  "http://www.pls.unibo.it/it/geologia",
-  "http://www.pls.unibo.it/it/informatica",
-  "http://www.pls.unibo.it/it/matematica",
-  "http://www.pls.unibo.it/it/scienze-naturali-ambientali",
-  "http://www.pls.unibo.it/it/statistica"
-]
-
 SOFIA_LINK = "https://sofia.istruzione.it/"
 
 module Pls
   class Application < Rails::Application
+    config.default_from = "Organizzazione PLS <notifica.inviodlist.08218@unibo.it>"
     config.archive_url_root = "https://www.dm.unibo.it/archivio-pls/it"
     config.project_hightlights = config.project_hightlights = [734, 742]
+    config.impersonate_admins = ["pietro.donatini@unibo.it", "valeria.montesi3@unibo.it"]
+    config.main_impersonations = ["mirella.manaresi@unibo.it", "nicola.arcozzi@unibo.it"]
   end
 end
