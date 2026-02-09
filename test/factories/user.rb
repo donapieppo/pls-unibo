d488eb51e51786143b2e75aa6afe06ba27c4d9d9
Pietro Donatini <pietro.donatini@unibo.it>
2026-02-09 12:25:32 +0100

    trait

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,7 +1,27 @@
 FactoryBot.define do
   factory :user do
-    sequence(:upn) { |n| "upn#{n}" }
-    add_attribute(:name) { "Pippo" }
-    add_attribute(:surname) { "Pluto" }
+    sequence(:email) { |n| "user#{n}@example.test" }
+    sequence(:name) { |n| "Name#{n}" }
+    sequence(:surname) { |n| "Surname#{n}" }
+    role { "other" }
+    staff { false }
+
+    trait :teacher do
+      role { "teacher" }
+      association :school
+    end
+
+    trait :student_secondary do
+      role { "student_secondary" }
+      association :school
+    end
+
+    trait :student_university do
+      role { "student_university" }
+    end
+
+    trait :staff do
+      staff { true }
+    end
   end
 end
